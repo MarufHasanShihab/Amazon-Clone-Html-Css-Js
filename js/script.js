6d2991978a17b8b0b3f48970ee56d797ed1251f8
Maruf Hasan Shihab <marufhasanshihab2@gmail.com>
2024-04-01 20:17:05 +0600

    compleate header slider

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// finding element
+const images = document.querySelectorAll(".header_slider ul img");
+const prevBtn = document.querySelector(".contral_prev");
+const nextBtn = document.querySelector(".contral_next");
+
+let n = 0;
+function slideChange(){
+    for(let image of images){
+        image.style.display = "none";
+    }
+    images[n].style.display = "block";
+}
+slideChange();
+
+prevBtn.addEventListener("click", ()=>{
+    if(n > 0){
+        n--;
+    }else{
+        n = images.length -1;
+    }
+    slideChange();
+})
+
+nextBtn.addEventListener("click", ()=>{
+    if(n < images.length -1){
+        n++;
+    }else{
+        n = 0;
+    }
+    slideChange();
+})
